1ab0280a47c620d41849d02e14762b28fddd6752
Swetha <swethavinuraj@gmail.com>
2026-06-23 20:17:49 +0530

    added register page and admin adding user page

--- src/main/java/com/project/SmartLeave/Controller/AdminController.java
+++ src/main/java/com/project/SmartLeave/Controller/AdminController.java
@@ -1,9 +1,12 @@
 package com.project.SmartLeave.Controller;
 import com.project.SmartLeave.Entity.User;
 import com.project.SmartLeave.Service.AdminService;
+import com.project.SmartLeave.Service.AuthService;
 import com.project.SmartLeave.dto.AdminDashboardResponse;
 import com.project.SmartLeave.dto.DashboardResponse;
+import com.project.SmartLeave.dto.RegisterRequest;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,10 +18,13 @@ public class AdminController {
 
 
     private final AdminService adminService;
+    private final AuthService authService;
 
     public AdminController(
-            AdminService adminService) {
+            AdminService adminService,
+            AuthService authService, AuthService authService1) {
         this.adminService = adminService;
+        this.authService = authService1;
     }
 
     @GetMapping("/users")
@@ -42,6 +48,16 @@ public class AdminController {
             @PathVariable Long id) {
 
         return adminService.deleteUser(id);
+    }
+    @PostMapping("/add-user")
+    public ResponseEntity<?> addUser(
+            @RequestBody RegisterRequest request) {
+
+        authService.register(request);
+
+        return ResponseEntity.ok(
+                "User Added Successfully"
+        );
     }
 
 }
